93f8127685ec8af0ddfa58a1d2627673ac421a86
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-01-04 04:11:41 -0800

    Done

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # create a Ruby script that accepts one argument and 
 # pass it to a regular expression matching method.
-puts ARGV[0].scan(/school/).join
+puts ARGV[0].scan(/School/).join
