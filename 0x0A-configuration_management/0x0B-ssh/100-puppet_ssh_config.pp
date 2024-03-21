5ca4cecbb1f2b56c82bb5f99682191b30f0fd9c5
mkkmsotafa12 <kamalmostaf2@gmail.com>
2024-03-21 17:30:33 -0400

    D

--- 0x0A-configuration_management/0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,7 +0,0 @@
-#file to create config file
-node default {
-exec {'echo':
-command => 'echo PasswordAuthentication no"\n"IdentityFile ~/.ssh/school >> /etc/ssh/ssh_config',
-path    => '/usr/bin'
-}
-}
